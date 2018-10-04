d115f8faa1ca813063ecede76f50040a986f25b0
Cristy Sillerico <cristysillerico@Cristys-MacBook-Pro.local>
2018-10-04 18:41:54 -0400

    Fix Merge Conflicts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Candidate from "./pages/Candidate";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 // import logo from './logo.svg';
@@ -14,6 +15,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
+        <Route exact path="/candidate" component={Candidate} />
         <Route component={NoMatch} />
       </Switch>
     </div>
