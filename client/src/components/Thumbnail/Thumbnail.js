9d32491fa69d405fe4ac7bc22212db8975e015af
Lysandra Owen <lysandraowen@gmail.com>
2018-10-19 21:36:56 -0400

    HomePage.js Link to Projects

---
+++ client/src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Thumbnail.css";
+
+// The Thumbnail component renders a div that uses some CSS to render a background image
+// It will always keep square proportions at any size without the image warping
+// The "role" and "aria label" are there to identify the element's purpose as an image for accessibility purposes
+const Thumbnail = ({ _id, project_name, project_image }) => (
+  <Link to="/recruiter">
+    <div 
+      className="thumbnail"
+      role="img"
+      aria-label="Project Image">   
+      <img alt={project_name} src={project_image} />
+      <p className="p_thumbnail">{project_name}</p>
+      <span onClick={(e) => this.hanldeGetProjectClick(_id, e)} className="project_detail"></span>
+    </div>
+  </Link>
+);
+
+export default Thumbnail;
